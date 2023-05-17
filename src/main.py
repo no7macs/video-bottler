8f19774f98a0249923f48c98069b98de5c614212
no7mac <44577325+no7macs@users.noreply.github.com>
2023-05-16 20:40:18 -0600

    lobed the slider into a class

--- src/main.py
+++ src/main.py
@@ -85,14 +85,32 @@ class encodeAndValue:
         #videoSize[0] = a if (a if (a:=((targetVideoBitrate/100)*145)) > 280 else 280) < videoSize[0] else videoSize[0]
         return(self.targetVideoWidth, self.targetVideoHeight)
 
-def bitrateRatioSliderUpdate(bitrateRatio):
-    #snapedRatio = min([0.1, 0.5, 0.6, 0.9],key=lambda x:abs(x-float(bitrateRatio)))
-    #bitrateRatioSlider.set(bitrateRatio)
-    testTest["text"] = str(bitrateRatio)
-    print(bitrateRatioSlider.coords())
-    print(bitrateRatioSlider.winfo_x())
-    audioBitrateLabel.place(x=(((bitrateRatioSlider.coords()[0])/2)))
-    videoBitrateLabel.place(x=((bitrateRatioSlider.coords()[0]+bitrateRatioSlider.cget("length")-videoBitrateLabel.winfo_width())/2))
+
+class bitrateSlider(Frame):
+    def __init__(self, *args, **kwargs):
+        Frame.__init__(self, *args, **kwargs)
+
+        self.bitrateSliderFrame = Frame(self)
+        self.bitrateSliderFrame.pack(anchor = W, expand=True, fill='both')
+        self.sliderValuesFrame = Frame(self.bitrateSliderFrame)
+        self.sliderValuesFrame.pack(anchor = W, expand=True, fill='both')
+        self.audioBitrateLabel = Label(self.sliderValuesFrame, text='0kb/s')
+        self.audioBitrateLabel.pack(side=LEFT)
+        self.videoBitrateLabel = Label(self.sliderValuesFrame, text='0kb/s')
+        self.videoBitrateLabel.pack(side=RIGHT)
+        self.bitrateRatio = DoubleVar()
+        self.bitrateRatioSlider = Scale(self.bitrateSliderFrame, orient=HORIZONTAL, from_=0, to=1, showvalue=0, variable=self.bitrateRatio, length=500, resolution=0.01, command=self.bitrateRatioSliderUpdate)
+        self.bitrateRatioSlider.pack(anchor = W)
+
+    def bitrateRatioSliderUpdate(self, bitrateRatio):
+        #snapedRatio = min([0.1, 0.5, 0.6, 0.9],key=lambda x:abs(x-float(bitrateRatio)))
+        #bitrateRatioSlider.set(bitrateRatio)
+        testTest["text"] = str(bitrateRatio)
+        print(self.bitrateRatioSlider.coords())
+        print(self.bitrateRatioSlider.winfo_x())
+        self.audioBitrateLabel.place(x=(((self.bitrateRatioSlider.coords()[0])/2)))
+        self.videoBitrateLabel.place(x=((self.bitrateRatioSlider.coords()[0]+self.bitrateRatioSlider.cget("length")-self.videoBitrateLabel.winfo_width())/2))
+
 
 if __name__ == "__main__":
     valueTings = encodeAndValue()
@@ -101,44 +119,32 @@ if __name__ == "__main__":
     targetVideoBitrate = valueTings.setTargetVideoBitrate()
     videoX, videoY = valueTings.setTargetVideoSize()
 
-    root = Tk()
-    root.title("Video Bottler")
+    #root = Tk()
+    #root.title("Video Bottler")
+    #slider = bitrateSlider(root).pack(anchor=W)
+    #testTest = Label(root, text='0')
+    #testTest.pack(anchor = W)
+    #root.mainloop()
 
-    bitrateSliderFrame = Frame(root)
-    bitrateSliderFrame.pack(anchor = W, expand=True, fill='both')
-    sliderValuesFrame = Frame(bitrateSliderFrame)
-    sliderValuesFrame.pack(anchor = W, expand=True, fill='both')
-    audioBitrateLabel = Label(sliderValuesFrame, text='0kb/s')
-    audioBitrateLabel.pack(side=LEFT)
-    videoBitrateLabel = Label(sliderValuesFrame, text='0kb/s')
-    videoBitrateLabel.pack(side=RIGHT)
-    bitrateRatio = DoubleVar()
-    bitrateRatioSlider = Scale(bitrateSliderFrame, orient=HORIZONTAL, from_=0, to=1, showvalue=0, variable=bitrateRatio, length=500, resolution=0.01, command=bitrateRatioSliderUpdate)
-    bitrateRatioSlider.pack(anchor = W)
-    
-    testTest = Label(root, text='0')
-    testTest.pack(anchor = W)
-    root.mainloop()
+    videoEncoder = "libvpx-vp9"
+    audioCodec = "libopus"
+    fileEnding = "webm"
 
-videoEncoder = "libvpx-vp9"
-audioCodec = "libopus"
-fileEnding = "webm"
+    videoPass1 = subprocess.run(["ffmpeg", "-y", "-i", file, "-b:v", f"{targetVideoBitrate}k",
+                                "-c:v",  videoEncoder,  "-maxrate", f"{(targetVideoBitrate/100)*80}k", 
+                                "-bufsize", f"{targetVideoBitrate*2}k", "-minrate", "0k",
+                                "-vf", f"scale={videoX}:{videoY}",
+                                "-deadline", "good", "-auto-alt-ref", "1", "-lag-in-frames", "24",
+                                "-threads", "0", "-row-mt", "1",
+                                "-pass", "1", "-an", "-f", "null", "NUL"])
 
-videoPass1 = subprocess.run(["ffmpeg", "-y", "-i", file, "-b:v", f"{targetVideoBitrate}k",
-                            "-c:v",  videoEncoder,  "-maxrate", f"{(targetVideoBitrate/100)*80}k", 
-                            "-bufsize", f"{targetVideoBitrate*2}k", "-minrate", "0k",
-                            "-vf", f"scale={videoX}:{videoY}",
-                            "-deadline", "good", "-auto-alt-ref", "1", "-lag-in-frames", "24",
-                            "-threads", "0", "-row-mt", "1",
-                            "-pass", "1", "-an", "-f", "null", "NUL"])
-
-videoPass2 = subprocess.run(["ffmpeg", "-y", "-i", file, "-b:v", f"{targetVideoBitrate}k",
-                            "-c:v", videoEncoder, "-maxrate", f"{(targetVideoBitrate/100)*80}k",
-                            "-bufsize", f"{targetVideoBitrate*2}k", "-minrate", "0k",
-                            "-vf", f"scale={videoX}:{videoY}",
-                            "-deadline", "good", "-auto-alt-ref", "1", "-lag-in-frames", "24",
-                            "-threads", "0", "-row-mt", "1",
-                            "-map_metadata", "0", "-metadata:s:v:0", f"bit_rate={targetVideoBitrate}",
-                            "-pass", "2", "-c:a", audioCodec, "-frame_duration", "20",
-                            "-b:a", f"{audioBitrate}k",
-                            f"{os.path.splitext(file)[0]}1.{fileEnding}"])
+    videoPass2 = subprocess.run(["ffmpeg", "-y", "-i", file, "-b:v", f"{targetVideoBitrate}k",
+                                "-c:v", videoEncoder, "-maxrate", f"{(targetVideoBitrate/100)*80}k",
+                                "-bufsize", f"{targetVideoBitrate*2}k", "-minrate", "0k",
+                                "-vf", f"scale={videoX}:{videoY}",
+                                "-deadline", "good", "-auto-alt-ref", "1", "-lag-in-frames", "24",
+                                "-threads", "0", "-row-mt", "1",
+                                "-map_metadata", "0", "-metadata:s:v:0", f"bit_rate={targetVideoBitrate}",
+                                "-pass", "2", "-c:a", audioCodec, "-frame_duration", "20",
+                                "-b:a", f"{audioBitrate}k",
+                                f"{os.path.splitext(file)[0]}1.{fileEnding}"])
